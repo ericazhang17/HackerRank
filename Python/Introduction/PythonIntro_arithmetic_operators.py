abcb41f360f51a2b69b2bf86a66a7d84ea3d8209
ericazhang17 <35720163+ericazhang17@users.noreply.github.com>
2018-05-09 03:08:29 -0400

    Add files via upload

---
+++ Python/Introduction/PythonIntro_arithmetic_operators.py
@@ -0,0 +1,11 @@
+#HackerRank arithmetic operators
+#Author: Erica Zhang 
+#Date: 5/9/18
+
+a = int(raw_input())
+b = int(raw_input())
+while a >=1 and a<= 10**10 and b>=1 and b<= 10**10:
+    print a+b
+    print a-b
+    print a*b
+    break
